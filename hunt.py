f0ee4d24f8ec32c59eff4e4a3e2ae81e5d23864e
Idafen Santana Pérez <idafensp@gmail.com>
2022-03-10 18:00:18 +0000

    Update hunt.py

--- hunt.py
+++ hunt.py
@@ -1,1 +1,22 @@
+//This is a sample python file
+
+def multiply(x,y):
+	return x - y
+
+def addition(x):
+	return x + y
+
+def beauty_print(x):
+print("The result is " + x)
+
+
+print("Starting the program...")
+
+m = multiply(2,4)
+beauty_print(m)
+
+a = addition(2,4)
+buty_pint(a)
+
+print("Ending the program...")
 
